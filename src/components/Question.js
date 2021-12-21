59ce85bc3714c4666a440aaf902a8106780d73bf
sofiaversoza <sofversoza@gmail.com>
2021-12-21 16:46:41 -0700

    commit tree 4a57b4ec9f1103045ef945f811eafa776bbf2072

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,10 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+    // set up a timeout to run after 1 sec
+  useEffect(() => {
+    const timer = setTimeout(() => {   // decrement the time remaining by 1
+      setTimeRemaining(timeRemaining => timeRemaining - 1)
+    }, 1000);
 
+    return function cleanup() {   // clean up after timeout func
+      clearTimeout(timer)
+    }
+  }, [timeRemaining, onAnswered]);  // was given on the console's warning mssg  
+
+
+  useEffect(() => {
+    if (timeRemaining === 0) {  // when time remaining hits 0
+      setTimeRemaining(10);     // reset time remaining back to 10
+      onAnswered(false);        // when the timer runs out-- answer will be false
+      return;                   // exit early
+    }
+  })
+
+  
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
     onAnswered(isCorrect);
